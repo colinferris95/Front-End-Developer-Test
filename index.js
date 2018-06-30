177f136435a8affe46f077447108b3fdf3392362
Colin Ferris <colinferris95@gmail.com>
2018-06-30 14:17:04 +0000

    refactor code, seperate app from index entry point

--- index.js
+++ index.js
@@ -2,105 +2,11 @@
 //imports
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-
-//default api with key attached for now
-const apiurl = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=edd3b79527fbe606b38fd4757220f789'
-
-
-const styles = {
-  app: {
-    paddingTop: 40,
-    textAlign: 'center',
-  },
-}
-
-//build the app
-class App extends Component {
-	
-	constructor(props) {
-    super(props)
-	//set the request as failed
-    this.state = {
-      requestFailed: false
-    }
-
-   
-	 this.handleSubmit = this.handleSubmit.bind(this);
-  }
-	//grabs the url and sets the data
-  componentDidMount() {
-    fetch(apiurl)
-      .then(res => res.json())
-	  .then(
-	  (result) => {
-		 this.setState({
-			weatherData:result
-	    });
-	  },
-	  (error) => {
-		  this.setState({
-			requestFailed : true ,
-			error
-		  });
-	  }
-	)
-	  
-  }
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/app';
 
 
-  
-  handleSubmit(e){
-	  //handle the form event to run the api function on form submit
-	  this.componentDidMount();
-	  
-  }
-	
-	
-	
-	
-	render() {
-		
-	if (this.state.requestFailed) return <p>Failed!</p>
-    if (!this.state.weatherData) return <p>Loading...</p>
-	
-	
-	
-		return (
-			<div>
-				<h2> call weather data </h2>
-				{/*form to call the api*/}
-				<form>
-					<input type="submit" value="Submit" onClick={this.handleSubmit} />
-				</form>
-				
-				
-				{/*api output goes here*/}
-				
-				<h2>{this.state.weatherData.name}</h2>
-				
-				<div>
-				{this.state.weatherData.weather.map(function(weather) {
-					return(
-					<div>
-					<p> {weather.main} </p>
-					<p> {weather.temp} </p>
-					</div>)
-				})}
-				
-				
-				
-				<a href="#"> more info... </a>
-				
-				</div>
-			
-				
-				
-				
-			</div>	
-		)
-	}
-}
 
 //select the html element and render the app
 const root = document.querySelector('#app')
-ReactDOM.render(<App />, root)
+ReactDOM.render( <App /> , root)
